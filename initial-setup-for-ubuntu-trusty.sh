c1acf65df67f14008a80ad3c01bbe4594d3d4ecc
YAMADA Hideki <yamada.hideki@gmail.com>
2016-01-21 06:48:12 +0000

    コメントアウトアウト
    Signed-off-by: YAMADA Hideki <yamada.hideki@gmail.com>

--- initial-setup-for-ubuntu-trusty.sh
+++ initial-setup-for-ubuntu-trusty.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -ex
 
-#sudo apt-get update
-#sudo apt-get upgrade -y 
-#sudo apt-get remove -y nano
-#sudo apt-get install -y vim 
-#sudo apt-get install -y build-essential
+sudo apt-get update
+sudo apt-get upgrade -y 
+sudo apt-get remove -y nano
+sudo apt-get install -y vim 
+sudo apt-get install -y build-essential
 
 # Sheepdog build dependencies
-#sudo apt-get install -y yasm autoconf dh-autoreconf pkg-config
-#sudo apt-get install -y liburcu-dev libzookeeper-mt-dev
-#sudo apt-get install -y libcorosync-dev
+sudo apt-get install -y yasm autoconf dh-autoreconf pkg-config
+sudo apt-get install -y liburcu-dev libzookeeper-mt-dev
+sudo apt-get install -y libcorosync-dev
 sudo apt-get install -y dh-systemd devscripts
