b34f046f3eceda82d037b611d78d43b5c8256155
Hideki Yamada <yamada.hideki@gmail.com>
2018-08-17 16:01:30 +0900

    もはや過去のものとなったファイル達

--- embulk/init.sql
+++
@@ -1,4 +0,0 @@
-create database dlog;
-create user 'dlog'@'localhost';
-grant all on dlog.* to 'dlog'@'localhost';
-
--- sheepdog/replace_libzookeeper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -x
-
-sudo rm /usr/lib/x86_64-linux-gnu/libzookeeper_mt.so.2
-sudo ln -s /usr/lib/x86_64-linux-gnu/libzookeeper_mt.so.2.0.0.a /usr/lib/x86_64-linux-gnu/libzookeeper_mt.so.2
-
-sudo rm /usr/lib/x86_64-linux-gnu/libzookeeper_mt.so
-sudo ln -s /usr/lib/x86_64-linux-gnu/libzookeeper_mt.so.2.0.0.a /usr/lib/x86_64-linux-gnu/libzookeeper_mt.so
